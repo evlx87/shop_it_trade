600160d013c3b66db41ebc75af55dd317efee090
evlx87 <evilnyagaming@yandex.ru>
2020-03-29 18:16:24 +0300

    add models and forms in authapp

---
+++ backend/apps/authapp/forms.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.forms import forms
+
+from backend.apps.authapp.models import User
+
+
+class UserLoginForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class UserRegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username',
+                  'first_name',
+                  'last_name',
+                  'password1',
+                  'password2',
+                  'email',
+                  'age',
+                  'avatar')
+
+    def __init__(self, *args, **kwargs):
+        super(UserRegisterForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
+
+    def clean_age(self):
+        data = self.cleaned_data['age']
+        if data < 18:
+            raise forms.ValidationError("Вы слишком молоды!")
+        return data
