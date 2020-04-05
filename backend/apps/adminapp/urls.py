9ffd5832637772cdb7d42c02123bb22c42d11e3b
evlx87 <evilnyagaming@yandex.ru>
2020-04-05 17:07:01 +0300

    add adminapp templates, forms and urls

---
+++ backend/apps/adminapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import re_path
+import backend.apps.adminapp.views as admin
+
+app_name = 'admin'
+
+urlpatterns = [
+    re_path(r'^$', admin.index, name='index'),
+
+    re_path(r'^user/create/$', admin.user_create, name='user_create'),
+    re_path(r'^user/update/(?P<pk>\d+)/$', admin.user_update, name='user_update'),
+    re_path(r'^user/delete/(?P<pk>\d+)/$', admin.user_delete, name='user_delete'),
+
+    re_path(r'^category/list/$', admin.productcategory_list, name='productcategory_list'),
+]
--- backend/apps/adminapp/views.py
+++ backend/apps/adminapp/views.py
@@ -1,3 +1,80 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
-# Create your views here.
+from backend.apps.adminapp.forms import UserAdminCreateForm, UserAdminUpdateForm
+from backend.apps.authapp.models import User
+from backend.apps.mainapp.models import ProductCategory
+
+
+def index(request):
+    object_list = User.objects.all()
+
+    context = {
+        'page_title': 'админка/пользователи',
+        'object_list': object_list
+    }
+    return render(request, 'adminapp/admin_panel.html', context)
+
+
+@user_passes_test(lambda x: x.is_superuser)
+def user_create(request):
+    if request.method == 'POST':
+        form = UserAdminCreateForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('adminapp:index'))
+    else:
+        form = UserAdminCreateForm()
+
+    context = {
+        'page_title': 'админка/новый пользователь',
+        'form': form
+    }
+    return render(request, 'adminapp/user_update.html', context)
+
+
+@user_passes_test(lambda x: x.is_superuser)
+def user_update(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    if request.method == 'POST':
+        form = UserAdminUpdateForm(request.POST, request.FILES, instance=user)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('adminapp:index'))
+    else:
+        form = UserAdminUpdateForm(instance=user)
+
+    context = {
+        'page_title': 'админка/редактирование пользователя',
+        'form': form
+    }
+    return render(request, 'adminapp/user_update.html', context)
+
+
+@user_passes_test(lambda x: x.is_superuser)
+def user_delete(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    if request.method == 'POST':
+        user.is_active = False
+        user.save()
+        return HttpResponseRedirect(reverse('adminapp:index'))
+    elif request.method == 'GET':
+        context = {
+            'page_title': 'Админка/удаление пользователя',
+            'object': user,
+        }
+        return render(request, 'adminapp/user_delete.html', context)
+
+
+@user_passes_test(lambda x: x.is_superuser)
+def productcategory_list(request):
+    object_list = ProductCategory.objects.all()
+
+    context = {
+        'page_title': 'Админка/категории',
+        'object_list': object_list,
+    }
+
+    return render(request, 'adminapp/productcategory_list.html', context)
