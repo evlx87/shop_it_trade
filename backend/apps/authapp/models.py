310619f886a835cd0e1d58552a7bbf2b509af2f1
evlx87 <evilnyagaming@yandex.ru>
2020-04-05 13:56:33 +0300

    correct def save_user_profile

--- backend/apps/authapp/models.py
+++ backend/apps/authapp/models.py
@@ -31,4 +31,4 @@ class UserProfile(models.Model):
 
     @receiver(post_save, sender=User)
     def save_user_profile(sender, instance, **kwargs):
-        instance.shopuserprofile.save()
+        instance.userprofile.save()
